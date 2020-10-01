0cced8fa96facc896bd8879eb3020d6bc327dc90
anh.tran(c0720i1) <69283798+theanh111@users.noreply.github.com>
2020-10-01 10:23:15 +0700

    thuc hanh Copy các file có dung lượng lớn?

---
+++ src/Main.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.Scanner;
+
+public class Main {
+    private static void copyFileUsingJava7Files(File source, File dest) throws IOException {
+        Files.copy(source.toPath(), dest.toPath(), StandardCopyOption.REPLACE_EXISTING);
+    }
+
+    private static void copyFileUsingStream(File source, File dest) throws IOException {
+        InputStream is = null;
+        OutputStream os = null;
+        try {
+            is = new FileInputStream(source);
+            os = new FileOutputStream(dest);
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = is.read(buffer)) > 0) {
+                os.write(buffer, 0, length);
+            }
+        } finally {
+            is.close();
+            os.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter source pathname: ");
+        String sourcePath = input.nextLine();
+        System.out.println("Enter destination pathname: ");
+        String destPath = input.nextLine();
+
+        File sourceFile = new File(sourcePath);
+        File destFile = new File(destPath);
+
+        try {
+            copyFileUsingJava7Files(sourceFile, destFile);
+            System.out.println("Copy Completed!");
+        } catch (IOException ex) {
+            System.out.println("Can't copy that file!");
+            System.out.println("[" + ex.getMessage() + "]");
+        }
+    }
+
+}
